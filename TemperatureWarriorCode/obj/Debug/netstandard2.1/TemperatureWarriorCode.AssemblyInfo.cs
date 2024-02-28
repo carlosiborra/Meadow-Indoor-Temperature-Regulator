6a3357aeb42b087f42fc166a3ba220aca9619a69
DaniFdz <danifernandezzzzzz@gmail.com>
2024-02-28 14:43:17 +0000

    Added gitignore

--- TemperatureWarriorCode/obj/Debug/netstandard2.1/TemperatureWarriorCode.AssemblyInfo.cs
+++ TemperatureWarriorCode/obj/Debug/netstandard2.1/TemperatureWarriorCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f32a6a1b43d7b81035d18119ce6cfdbcc6f1ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
