0ca0fb2f6b373ba4935af34207c0add75f69407b
pablohd10 <phidalgod10@gmail.com>
2024-04-23 21:56:09 +0200

    MUESTRA DE COMO SE UTILIZARIA EL CONTROLADOR PID. ADAPTAR A NUESTRO CÓDIGO DEL PROYECTO (la manera en que se reciben los parámetros y la temperatura actual y la manera en que se aplica la salida del PID al sistema)

--- TemperatureWarriorCode/PIDController/PIDControllerUsage.cs
+++ TemperatureWarriorCode/PIDController/PIDControllerUsage.cs
@@ -34,13 +34,11 @@ Stopwatch stopwatch = new Stopwatch(); // Create a stopwatch to measure the time
 for (int i = 0; i < duracion_rangos.Length; i++) {
     double temperatura_objetivo = (rangosTemperaturas[i][0] + rangosTemperaturas[i][1]) / 2.0 ; // Calcular la temperatura objetivo como el promedio de la temperatura mínima y máxima del rango
     stopwatch.Start(); // Start the stopwatch
+
     while (stopwatch.Elapsed.TotalSeconds < duracion_rangos[i]) // While the elapsed time is less than the duration of the specific range (in seconds)
       {
-      // Obtener la temperatura actual
       double actualTemperature = obtenerTemperaturaActual(); // Obtaining the current temperature. Adapt this funcion to the one used in the project
-      // Calcular la salida del controlador PID
-      double output = controller.Compute(actualTemperature, temperatura_objetivo);
-      // Aplicar la salida del controlador PID al sistema
+      double output = controller.Compute(actualTemperature, temperatura_objetivo); // Compute the PID controller output based on the current temperature and the target temperature
       aplicarSalidaControlador(output); // Applying the PID controller output to the system. Adapt this function to the one used in the project
       }
 
