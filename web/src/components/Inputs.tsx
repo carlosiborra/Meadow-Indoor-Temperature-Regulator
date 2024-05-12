49926eb916b88d5de431a7996758aee6a0eae3c5
DaniFdz <danifernandezzzzzz@gmail.com>
2024-05-12 16:18:41 +0000

    Added toast's info

--- web/src/components/Inputs.tsx
+++ web/src/components/Inputs.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
+import { useToast } from './ui/use-toast';
 
 const Inputs = () => {
   const [minTemperature, setMinTemperature] = useState('');
@@ -7,28 +8,61 @@ const Inputs = () => {
   const [roundDuration, setRoundDuration] = useState('');
   const [internalRate, setInternalRate] = useState('');
   const [refreshRate, setRefreshRate] = useState('');
+  const { toast } = useToast()
 
-  const validateInputs = () => {
-    const tempValidFormat = /^((1[2-9]|2[0-9]|30);)*(1[2-9]|2[0-9]|30);?$/; // Números del 12 al 30 seguidos de ';'
-    const genericValidFormat = /^(\d+;)*\d+;?$/; // Cualquier número seguido de ';'
-    
+  function validateInputs(): boolean {
+    const tempValidFormat = /^(1[2-9]|2[0-9]|30)(;(1[2-9]|2[0-9]|30))*$/; // Números del 12 al 30 seguidos de ';'
+    const genericValidFormat = /^\d+(;\d+)*$/; // Cualquier número seguido de ';'
+
     const minValid = tempValidFormat.test(minTemperature);
     const maxValid = tempValidFormat.test(maxTemperature);
     const roundValid = genericValidFormat.test(roundDuration);
-    
+
     const minValues = minTemperature.split(';').map(Number);
     const maxValues = maxTemperature.split(';').map(Number);
-    
+
     const allTempsValid = minValues.every((min, index) => min < maxValues[index]);
 
     const entries = [minTemperature, maxTemperature, roundDuration];
     const allSameLength = new Set(entries.map(entry => entry.split(';').filter(Boolean).length)).size === 1;
 
-    if (!minValid || !maxValid || !roundValid || !allSameLength || !allTempsValid) {
-      alert('Uno o más campos están mal escritos, no tienen la misma cantidad de elementos, no están en el rango de 12 a 30, o los valores mínimos no son menores que los máximos correspondientes.');
-    } else {
-      alert('Todos los datos son correctos.');
+    if (!minValid) {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: 'La temperatura mínima no tiene un formato válido'
+      })
+      return false;
+    } if (!maxValid) {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: 'La temperatura máxima no tiene un formato válido'
+      })
+      return false;
+    } if (!roundValid) {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: 'Los números de ronda no tienen un formato válido',
+      })
+      return false;
+    } if (!allSameLength) {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: 'Las 3 primeras entradas deben tener la misma cantidad de valores',
+      })
+      return false;
+    } if (!allTempsValid) {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: 'Las temperaturas no son válidas, recuerda que la temperatura mínima debe ser menor a la máxima.',
+      })
+      return false;
     }
+    return true;
   };
 
   return (
@@ -43,7 +77,7 @@ const Inputs = () => {
         className={twMerge("col-start-2 input text-fountain-blue-500 placeholder-fountain-blue-opacity-50")}
         placeholder="12; 15; 14..."
       />
-      
+
       <span className="col-start-1">
         Max Temperature (°C):
       </span>
@@ -54,7 +88,7 @@ const Inputs = () => {
         className={twMerge("col-start-2 input text-guardsman-red-500 placeholder-guardsman-red-opacity-50")}
         placeholder="25; 29; 30..."
       />
-      
+
       <span className="col-start-1">
         Round Duration (s):
       </span>
@@ -65,9 +99,9 @@ const Inputs = () => {
         className={twMerge("col-start-2 input")}
         placeholder="15; 20; 34..."
       />
-      
+
       <div style={{ margin: '20px 0', background: 'white', height: '2px', gridColumn: "span 2" }}></div>
-      
+
       <span className="col-start-1">
         Internal Rate (ms):
       </span>
@@ -78,7 +112,7 @@ const Inputs = () => {
         className={twMerge("col-start-2 input")}
         placeholder='150'
       />
-      
+
       <span className="col-start-1">
         Refresh Rate (ms):
       </span>
@@ -89,7 +123,7 @@ const Inputs = () => {
         className={twMerge("col-start-2 input")}
         placeholder='2000'
       />
-      
+
       <button className="col-start-2 mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={validateInputs}>
         Enviar
       </button>
